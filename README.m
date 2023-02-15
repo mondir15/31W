5a5fa2bb26569362f0034068dab5c4540ff25178
mondir15 <124097911+mondir15@users.noreply.github.com>
2023-02-15 08:21:12 -0500

    Update README.m

--- README.m
+++ README.m
@@ -1,2 +1,9 @@
 # 31W
 Exrecice1
+Installation de Wordpress
+Création d'un thème simple
+Utilisation d'une structure Sass simple
+Création d'un dépôt git pour le thème
+Création d'un dépôt Github
+Déployer un site Wordpress sur un serveur distan
+https://aecweb3.sg-host.com/
