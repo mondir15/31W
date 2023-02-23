52f5a1335697ca7a5a41579578aecb6ac9099720
mondir <mondirbenz18@gmail.com>
2023-02-23 14:58:26 -0500

    c8 intégration de l'engin de recherche et formulaire de recherchec8 intégration de l'engin de recherche et formulaire de recherche

---
+++ search.php
@@ -0,0 +1,28 @@
+<?php
+/**
+    Modèle search.php pour afficher les resultats de recherche
+*/
+get_header() ?>
+<main>
+    <code>search.php</code>
+    <sectiom class="recherche">
+<?php 
+if (have_posts()):
+    while (have_posts()) : the_post();
+        //the_title('<h1>','</h1>');
+        //the_permalink(); ?>
+        <article>
+        <h1><a href="<?php the_permalink(); ?>"><?= get_the_title();  ?> </a></h1>
+
+      
+        <?= wp_trim_words(get_the_excerpt(), 4);?>
+        
+        </article>
+        <?php
+    endwhile;
+endif;
+?>  
+</sectiom> 
+</main> 
+<?php get_footer(); ?>
+
