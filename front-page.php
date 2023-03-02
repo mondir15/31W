082ee660fdeda78b650d48dc27d3e830e0ed012d
mondir <mondirbenz18@gmail.com>
2023-03-02 15:21:53 -0500

    10 introduction a un template-parts

--- front-page.php
+++ front-page.php
@@ -9,13 +9,9 @@ get_header() ?>
     <section class="blocflex">
         <?php 
         if (have_posts()):
-            while (have_posts()) : the_post();
-                //the_title('<h1>','</h1>');
-                //the_permalink(); ?>
-                <article>
-                    <h3><a href="<?php the_permalink(); ?>"><?= get_the_title();  ?> </a></h3>
-                    <p><?php  echo wp_trim_words(get_the_excerpt(), 4); ?></p>
-                </article>
+            while (have_posts()) : the_post(); ?>
+        <?php get_template_part("templete/categorie", "note-wp");?>
+                
             <?php endwhile; ?>
         <?php endif; ?>   
     </section>
