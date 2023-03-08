a7104c209ad70c47c470bc08139ae9469a205eb2
mondir <mondirbenz18@gmail.com>
2023-03-08 08:35:08 -0500

    c11 modifier template-parts

--- front-page.php
+++ front-page.php
@@ -10,11 +10,12 @@ get_header() ?>
         <?php 
         if (have_posts()):
             while (have_posts()) : the_post(); ?>
-           <?php if (in_category('galerie')){
-                get_template_part("template-parts/categorie", "galerie");
-           }else{
-            get_template_part("template-parts/categorie", "note-wp");
+           <?php 
+           $ma_categorie = "note-wp";
+           if (in_category('galerie')){
+                $ma_categorie = "galerie";
            }
+            get_template_part("template-parts/categorie",$ma_categorie);
                  
              endwhile; ?>
         <?php endif; ?>   
---
+++ template-parts/aside.php
@@ -0,0 +1,19 @@
+<aside class="site__aside">
+
+<h3>menu secondaire</h3>
+
+<?php
+$category = get_queried_object();
+if (isset($category))
+{
+ $lemenu = $category->slug;
+}else{
+ $lemenu = "note-wp";
+}
+ wp_nav_menu(array(
+ "menu" => $lemenu,
+ "container" => "nav"
+
+))?>
+ </aside>
+ 
