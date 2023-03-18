6137222cc9f8e29d8217a44eca3839d8cd533872
mondir <mondirbenz18@gmail.com>
2023-03-18 10:22:46 -0400

    c14 Modifications style du theme

--- footer.php
+++ footer.php
@@ -4,7 +4,9 @@
 */
 ?>
 <footer class="site__footer">
-<h3>Pied de page</h3>
+<!--<h3>Pied de page</h3>-->
+<h3>31W - Un thème WordPress créatif</h3>
+<h3></h3>
 </footer>
 <?php wp_footer(); ?>
 </body>
