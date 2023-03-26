7cd45dcf19cf2ff94e7bb6c35a2a4dd580f1a42c
mondir <mondirbenz18@gmail.com>
2023-03-26 09:46:03 -0400

    c16 formatage de menu et background

--- header.php
+++ header.php
@@ -1,34 +1,86 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <?php wp_head(); ?>
-</head>
-<body class="custom-background site <?= (is_front_page() ? "no-aside" : "");  ?> ">
-    <header class="site__entete">  
-        <section class="logomenu">
-            <?php the_custom_logo(); ?> 
-            <div class="menusearch">
-                <input type="checkbox" id="chkBurger">
-                <?php wp_nav_menu(array(
-                    'menu' => 'entete',
-                    'container' => 'nav'
-                )); ?> 
-                <?= get_search_form(); ?>
-                <label for="chkBurger" class="burger">
-                <img src="https://s2.svgbox.net/hero-outline.svg?ic=menu&color=000" width="32" height="32">
-                </label>   
-            </div> 
-        </section>
-        <h1 class="site__titre"><a href="<?php  bloginfo('url'); ?>"><?php  bloginfo('name'); ?></a></h1> 
-        <h2 class="site__soutitre"><?php  bloginfo('description'); ?></h2>
-    </header>
-    <?php 
-    if (is_front_page() == false)
-    {
-       get_template_part("template-parts/aside");
+<?php
+// Enfiler la feuille de style
+function ajouter_styles() {
+    wp_enqueue_style('31w-style-principal', // id de la feuille de style
+                get_template_directory_uri() . '/style.css', // adresse url de la feuille de style
+                array(), // les dépendances avec les autres feuilles de style
+                filemtime(get_template_directory() . '/style.css')); // la de la dernière feuille de style
+}
+add_action( 'wp_enqueue_scripts', 'ajouter_styles' );
+
+/* ------------------------------------------------- Enregistrement des menus */
+
+function enregistrement_des_menus(){
+    register_nav_menus( array(
+        'menu_entete' => 'Menu entête' ,
+        'menu_footer'  => 'Menu pied de page',
+    ) );
+}
+add_action( 'after_setup_theme', 'enregistrement_des_menus', 0 );
+
+/**
+ * Modifie la requete principale de Wordpress avant qu'elle soit exécuté
+ * le hook « pre_get_posts » se manifeste juste avant d'exécuter la requête principal
+ * Dépendant de la condition initiale on peut filtrer un type particulier de requête
+ * Dans ce cas çi nous filtrons la requête de la page d'accueil
+ * @param WP_query  $query la requête principal de WP
+ */
+function cidweb_modifie_requete_principal( $query ) {
+    if (    $query->is_home() // si page d'accueil
+            && $query->is_main_query() // si requête principale
+            && ! is_admin() ) { // non tableau de bord
+      $query->set( 'category_name', 'note-wp' ); // filtre les articles de categorie «note-wp»
+      $query->set( 'orderby', 'title' );// trie selon le titre
+      $query->set( 'order', 'ASC' ); // en ordre ascendant
+      }
+     }
+     add_action( 'pre_get_posts', 'cidweb_modifie_requete_principal' );
+
+/**
+ * Permet de modifier les titre du menu « cours »
+ * @param $title : titre du choix menu
+ * @param $item : le choix global
+ * @param $args: Object qui représente la structure de menu 
+ * 
+ */
+
+
+
+
+/* --------------------------------------------------- Add_theme_support */
+add_theme_support( 'html5', 
+                    array(  'search-form',
+                            'gallery', 
+                            'caption' 
+                    ) );
+
+add_theme_support( 'title-tag' );
+
+add_theme_support( 'custom-logo', 
+                    array(
+                        'height' => 150,
+                        'width'  => 150,
+                    ) );
+
+
+ add_theme_support('custom-background');                   
+
+function perso_menu_item_title($title, $item, $args) {
+    // Remplacer 'nom_de_votre_menu' par l'identifiant de votre menu  
+  
+    if($args->menu == 'cours') {
+    // Modifier la longueur du titre en fonction de vos besoins
+    $sigle = substr($title, 4,3);
+    $title = substr($title, 7);
+    $title = "<div class='cours__sigle'>" . $sigle . "</div>" . 
+              "<p class='cours__titre'>" . wp_trim_words($title, 2, ' ... ') . "</p>";
+        
     }
-     ?>
+    if($args->menu == 'note-wp') {
+        if (substr($title,0,1) == '0') {
+            $title= substr($title,1);
+        }
+    }
+    return  $title ;    
+}
+add_filter('nav_menu_item_title', 'perso_menu_item_title', 10, 3);
--- readme.md
+++ readme.md
@@ -3,16 +3,34 @@
 
 ### Auteur: El Moundhir Benzeghioua
 
-# Exercice #3 - Widget et ACF
+# Exercice #3 (5 points)
 
-###### L\'objectif du Exercice 03:
+###### L\'objectif de l'exercice #3:
 
-Poursuivre le développement de thème sur le serveur distant.
-Produire 10 commits
-Créer de nouveaux templates 
-Intégrer des champs personnalisée
-Utiliser la structure de widget
+- Ajouter, gérer et utiliser des champs personnalisés
+- Adapter le menu aside et le menu de l'entete 
+- Déployer votre projet sur le serveur **_Siteground_**
+
+###### L'exercice vous permettra de vous familiariser avec les champs personnalisés
+
+- Cet exercice est un premiere étape de développement du TP2
 
 
-######:
+###### Déroulement du projet
+
+> Les « **_commits_** » seront poussés dans le dépôt **_31w_**  de votre github. Dans la branche **_exer3_**
+> L\'ensemble des commits devra s\'étaler progressivement à partir de la journée de remise du devis jusqu\'à la remise finale de l'exercice #3 
+> Un minimum de 5 commits pour le thème  permettra d\'évaluer l\'évolution de votre projet
+
+## Barème de correction sur 5 points
+
+1. Organisation des commits sur Github minimum 5 commits  pour le thème **_(1 points)_**
+
+   - Minimum de 5 commits étalés sur trois jours pour le thème 
+   - Messages clairs
+   - Déploiement de votre site sur Siteground
+   - Un readme.md pour le thème: auteur, titre du projet et description
+
+2. Utilisation et fonctionnement des champs personnalisé **_(2 points)_**
+3. Création de template-parts pour single.php « single-cours.php » et « single-note.php » pour afficher un article cours ou note-cours **_(2 points)_** 
 
