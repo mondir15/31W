dc7d361855227132f1de97defc774ae39f57dc95
mondir <mondirbenz18@gmail.com>
2023-04-06 15:21:50 -0400

    c19 ajouter footer et les evenements

--- footer.php
+++ footer.php
@@ -17,8 +17,39 @@
         <div></div>
         <div></div>
     </section>
+    <section class="footer__container">
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h4>Liens utiles</h4>
+        <ul>
+          <li><a href="#">Accueil</a></li>
+          <li><a href="#">A propos</a></li>
+          <li><a href="https://github.com/mondir15">Lien github</a></li>
+        </ul>
+      </div>
+      <div class="col">
+        <h4>Ressources</h4>
+        <ul>
+          <li><a href="#">Documentation</a></li>
+          <li><a href="#">FAQ</a></li>
+          <li><a href="#">Support</a></li>
+        </ul>
+      </div>
+      <div class="col">
+        <h4>Suivez-nous</h4>
+        <ul>
+          <li><a href="#">Facebook</a></li>
+          <li><a href="#">Twitter</a></li>
+          <li><a href="#">Instagram</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</section>
+
 <!--<h3>Pied de page</h3>-->
-<a href="https://github.com/mondir15">Lien github</a>
+
 <h3></h3>
 </footer>
 <?php wp_footer(); ?>
