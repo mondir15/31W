e5a05cd9b51125d06c44557c07dc11ace389d796
mondir <mondirbenz18@gmail.com>
2023-04-18 14:23:00 -0400

    EpreuveFinale cree readme

--- 404.php
+++ 404.php
@@ -5,7 +5,13 @@
 get_header() ?>
 <main class="site__main">
     <code>404.php</code>
-    <h1>Erreur 404 - la page n'existe pas</h1>
+    <h1>Erreur 404</h1>
+    <p>Page introuvable,vous pouvez tenter une recherche</p>
+    <?= get_search_form(); ?>
+                <label for="chkBurger" class="burger">
+                <img src="https://s2.svgbox.net/hero-outline.svg?ic=menu&color=000" width="32" height="32">
+                </label>  
+                <h3>Nos choix de cours</h3> 
 </main> 
 <?php get_footer(); ?>
 
